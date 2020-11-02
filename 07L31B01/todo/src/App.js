27e614fd3c0e8e6837d828bd26840c0cc693d643
Ramon Snellink <r.snellink@gmail.com>
2020-11-02 20:27:48 +0100

    added onChange

--- 07L31B01/todo/src/App.js
+++ 07L31B01/todo/src/App.js
@@ -1,3 +1,12 @@
+/**
+
+ * Challenge: 
+ * 1. Create an event handler in the App component for when the checkbox is clicked (which is an `onChange` event)
+ *    a. This method will be the trickest part. Check the comments in the stubbed-out method below for some pseudocode to help guide you through this part
+ * 2. Pass the method down to the TodoItem component
+ * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id of the todo into the function
+ */
+
 import React from "react";
 import todosData from "./todosData";
 import TodoItem from "./components/TodoItem";
@@ -6,11 +15,35 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = { todos: todosData };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatesTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+      return { todos: updatesTodos };
+    });
   }
 
   render() {
     const todoList = this.state.todos.map((todo) => {
-      return <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />;
+      return (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          completed={todo.completed}
+          handleChange={this.handleChange}
+        />
+      );
     });
 
     return <div className="todolist">{todoList}</div>;
--- 07L31B01/todo/src/components/TodoItem.js
+++ 07L31B01/todo/src/components/TodoItem.js
@@ -3,7 +3,12 @@ import React from "react";
 const TodoItem = (props) => {
   return (
     <div className="todo">
-      <input className="todo__checkbox" type="checkbox" checked={props.completed} />
+      <input
+        className="todo__checkbox"
+        type="checkbox"
+        checked={props.completed}
+        onChange={() => props.handleChange(props.id)}
+      />
       <p className="todo__text">{props.text}</p>
     </div>
   );
